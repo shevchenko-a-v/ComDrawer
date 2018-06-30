ffb51f397e0eb4c359eea01ee05d4e64fab6eead
shevchenko-a-v <38935246+shevchenko-a-v@users.noreply.github.com>
2018-06-30 20:23:42 +0300

    Created server dll

--- DrawerServer/DrawerServer_i.h
+++ DrawerServer/DrawerServer_i.h
@@ -30,6 +30,10 @@
 #error this stub requires an updated version of <rpcndr.h>
 #endif /* __RPCNDR_H_VERSION__ */
 
+#ifndef COM_NO_WINDOWS_H
+#include "windows.h"
+#include "ole2.h"
+#endif /*COM_NO_WINDOWS_H*/
 
 #ifndef __DrawerServer_i_h__
 #define __DrawerServer_i_h__
@@ -40,13 +44,177 @@
 
 /* Forward Declarations */ 
 
+#ifndef __ISquareObject_FWD_DEFINED__
+#define __ISquareObject_FWD_DEFINED__
+typedef interface ISquareObject ISquareObject;
+
+#endif 	/* __ISquareObject_FWD_DEFINED__ */
+
+
+#ifndef __SquareObject_FWD_DEFINED__
+#define __SquareObject_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class SquareObject SquareObject;
+#else
+typedef struct SquareObject SquareObject;
+#endif /* __cplusplus */
+
+#endif 	/* __SquareObject_FWD_DEFINED__ */
+
+
 /* header files for imported files */
 #include "oaidl.h"
 #include "ocidl.h"
+#include "shobjidl.h"
 
 #ifdef __cplusplus
 extern "C"{
 #endif 
+
+
+#ifndef __ISquareObject_INTERFACE_DEFINED__
+#define __ISquareObject_INTERFACE_DEFINED__
+
+/* interface ISquareObject */
+/* [unique][nonextensible][dual][uuid][object] */ 
+
+
+EXTERN_C const IID IID_ISquareObject;
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("ba0a089b-b7a5-4f3b-aa02-5679730cbd29")
+    ISquareObject : public IDispatch
+    {
+    public:
+        virtual HRESULT STDMETHODCALLTYPE SetPosition( 
+            /* [in] */ int xPos,
+            /* [in] */ int yPos,
+            /* [in] */ int sideSize) = 0;
+        
+        virtual HRESULT STDMETHODCALLTYPE Draw( 
+            /* [in] */ wireHDC hDc) = 0;
+        
+    };
+    
+    
+#else 	/* C style interface */
+
+    typedef struct ISquareObjectVtbl
+    {
+        BEGIN_INTERFACE
+        
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            ISquareObject * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            ISquareObject * This);
+        
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            ISquareObject * This);
+        
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )( 
+            ISquareObject * This,
+            /* [out] */ UINT *pctinfo);
+        
+        HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )( 
+            ISquareObject * This,
+            /* [in] */ UINT iTInfo,
+            /* [in] */ LCID lcid,
+            /* [out] */ ITypeInfo **ppTInfo);
+        
+        HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )( 
+            ISquareObject * This,
+            /* [in] */ REFIID riid,
+            /* [size_is][in] */ LPOLESTR *rgszNames,
+            /* [range][in] */ UINT cNames,
+            /* [in] */ LCID lcid,
+            /* [size_is][out] */ DISPID *rgDispId);
+        
+        /* [local] */ HRESULT ( STDMETHODCALLTYPE *Invoke )( 
+            ISquareObject * This,
+            /* [annotation][in] */ 
+            _In_  DISPID dispIdMember,
+            /* [annotation][in] */ 
+            _In_  REFIID riid,
+            /* [annotation][in] */ 
+            _In_  LCID lcid,
+            /* [annotation][in] */ 
+            _In_  WORD wFlags,
+            /* [annotation][out][in] */ 
+            _In_  DISPPARAMS *pDispParams,
+            /* [annotation][out] */ 
+            _Out_opt_  VARIANT *pVarResult,
+            /* [annotation][out] */ 
+            _Out_opt_  EXCEPINFO *pExcepInfo,
+            /* [annotation][out] */ 
+            _Out_opt_  UINT *puArgErr);
+        
+        HRESULT ( STDMETHODCALLTYPE *SetPosition )( 
+            ISquareObject * This,
+            /* [in] */ int xPos,
+            /* [in] */ int yPos,
+            /* [in] */ int sideSize);
+        
+        HRESULT ( STDMETHODCALLTYPE *Draw )( 
+            ISquareObject * This,
+            /* [in] */ wireHDC hDc);
+        
+        END_INTERFACE
+    } ISquareObjectVtbl;
+
+    interface ISquareObject
+    {
+        CONST_VTBL struct ISquareObjectVtbl *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define ISquareObject_QueryInterface(This,riid,ppvObject)	\
+    ( (This)->lpVtbl -> QueryInterface(This,riid,ppvObject) ) 
+
+#define ISquareObject_AddRef(This)	\
+    ( (This)->lpVtbl -> AddRef(This) ) 
+
+#define ISquareObject_Release(This)	\
+    ( (This)->lpVtbl -> Release(This) ) 
+
+
+#define ISquareObject_GetTypeInfoCount(This,pctinfo)	\
+    ( (This)->lpVtbl -> GetTypeInfoCount(This,pctinfo) ) 
+
+#define ISquareObject_GetTypeInfo(This,iTInfo,lcid,ppTInfo)	\
+    ( (This)->lpVtbl -> GetTypeInfo(This,iTInfo,lcid,ppTInfo) ) 
+
+#define ISquareObject_GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId)	\
+    ( (This)->lpVtbl -> GetIDsOfNames(This,riid,rgszNames,cNames,lcid,rgDispId) ) 
+
+#define ISquareObject_Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr)	\
+    ( (This)->lpVtbl -> Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr) ) 
+
+
+#define ISquareObject_SetPosition(This,xPos,yPos,sideSize)	\
+    ( (This)->lpVtbl -> SetPosition(This,xPos,yPos,sideSize) ) 
+
+#define ISquareObject_Draw(This,hDc)	\
+    ( (This)->lpVtbl -> Draw(This,hDc) ) 
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+
+#endif 	/* __ISquareObject_INTERFACE_DEFINED__ */
 
 
 
@@ -58,6 +226,14 @@ extern "C"{
 
 
 EXTERN_C const IID LIBID_DrawerServerLib;
+
+EXTERN_C const CLSID CLSID_SquareObject;
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("6cae19e1-61f7-4145-8026-45e985c86358")
+SquareObject;
+#endif
 #endif /* __DrawerServerLib_LIBRARY_DEFINED__ */
 
 /* Additional Prototypes for ALL interfaces */
---
+++ DrawerServer/DrawerServer_p.c
@@ -0,0 +1,374 @@
+
+
+/* this ALWAYS GENERATED file contains the proxy stub code */
+
+
+ /* File created by MIDL compiler version 8.01.0622 */
+/* at Tue Jan 19 05:14:07 2038
+ */
+/* Compiler settings for DrawerServer.idl:
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.01.0622 
+    protocol : dce , ms_ext, c_ext, robust
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+#if !defined(_M_IA64) && !defined(_M_AMD64) && !defined(_ARM_)
+
+
+#if _MSC_VER >= 1200
+#pragma warning(push)
+#endif
+
+#pragma warning( disable: 4211 )  /* redefine extern to static */
+#pragma warning( disable: 4232 )  /* dllimport identity*/
+#pragma warning( disable: 4024 )  /* array to pointer mapping*/
+#pragma warning( disable: 4152 )  /* function/data pointer conversion in expression */
+#pragma warning( disable: 4100 ) /* unreferenced arguments in x86 call */
+
+#pragma optimize("", off ) 
+
+#define USE_STUBLESS_PROXY
+
+
+/* verify that the <rpcproxy.h> version is high enough to compile this file*/
+#ifndef __REDQ_RPCPROXY_H_VERSION__
+#define __REQUIRED_RPCPROXY_H_VERSION__ 475
+#endif
+
+
+#include "rpcproxy.h"
+#ifndef __RPCPROXY_H_VERSION__
+#error this stub requires an updated version of <rpcproxy.h>
+#endif /* __RPCPROXY_H_VERSION__ */
+
+
+#include "DrawerServer_i.h"
+
+#define TYPE_FORMAT_STRING_SIZE   27                                
+#define PROC_FORMAT_STRING_SIZE   85                                
+#define EXPR_FORMAT_STRING_SIZE   1                                 
+#define TRANSMIT_AS_TABLE_SIZE    0            
+#define WIRE_MARSHAL_TABLE_SIZE   0            
+
+typedef struct _DrawerServer_MIDL_TYPE_FORMAT_STRING
+    {
+    short          Pad;
+    unsigned char  Format[ TYPE_FORMAT_STRING_SIZE ];
+    } DrawerServer_MIDL_TYPE_FORMAT_STRING;
+
+typedef struct _DrawerServer_MIDL_PROC_FORMAT_STRING
+    {
+    short          Pad;
+    unsigned char  Format[ PROC_FORMAT_STRING_SIZE ];
+    } DrawerServer_MIDL_PROC_FORMAT_STRING;
+
+typedef struct _DrawerServer_MIDL_EXPR_FORMAT_STRING
+    {
+    long          Pad;
+    unsigned char  Format[ EXPR_FORMAT_STRING_SIZE ];
+    } DrawerServer_MIDL_EXPR_FORMAT_STRING;
+
+
+static const RPC_SYNTAX_IDENTIFIER  _RpcTransferSyntax = 
+{{0x8A885D04,0x1CEB,0x11C9,{0x9F,0xE8,0x08,0x00,0x2B,0x10,0x48,0x60}},{2,0}};
+
+
+extern const DrawerServer_MIDL_TYPE_FORMAT_STRING DrawerServer__MIDL_TypeFormatString;
+extern const DrawerServer_MIDL_PROC_FORMAT_STRING DrawerServer__MIDL_ProcFormatString;
+extern const DrawerServer_MIDL_EXPR_FORMAT_STRING DrawerServer__MIDL_ExprFormatString;
+
+
+extern const MIDL_STUB_DESC Object_StubDesc;
+
+
+extern const MIDL_SERVER_INFO ISquareObject_ServerInfo;
+extern const MIDL_STUBLESS_PROXY_INFO ISquareObject_ProxyInfo;
+
+
+
+#if !defined(__RPC_WIN32__)
+#error  Invalid build platform for this stub.
+#endif
+#if !(TARGET_IS_NT60_OR_LATER)
+#error You need Windows Vista or later to run this stub because it uses these features:
+#error   forced complex structure or array, new range semantics, compiled for Windows Vista.
+#error However, your C/C++ compilation flags indicate you intend to run this app on earlier systems.
+#error This app will fail with the RPC_X_WRONG_STUB_VERSION error.
+#endif
+
+
+static const DrawerServer_MIDL_PROC_FORMAT_STRING DrawerServer__MIDL_ProcFormatString =
+    {
+        0,
+        {
+
+	/* Procedure SetPosition */
+
+			0x33,		/* FC_AUTO_HANDLE */
+			0x6c,		/* Old Flags:  object, Oi2 */
+/*  2 */	NdrFcLong( 0x0 ),	/* 0 */
+/*  6 */	NdrFcShort( 0x7 ),	/* 7 */
+/*  8 */	NdrFcShort( 0x14 ),	/* x86 Stack size/offset = 20 */
+/* 10 */	NdrFcShort( 0x18 ),	/* 24 */
+/* 12 */	NdrFcShort( 0x8 ),	/* 8 */
+/* 14 */	0x44,		/* Oi2 Flags:  has return, has ext, */
+			0x4,		/* 4 */
+/* 16 */	0x8,		/* 8 */
+			0x41,		/* Ext Flags:  new corr desc, has range on conformance */
+/* 18 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 20 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 22 */	NdrFcShort( 0x0 ),	/* 0 */
+
+	/* Parameter xPos */
+
+/* 24 */	NdrFcShort( 0x48 ),	/* Flags:  in, base type, */
+/* 26 */	NdrFcShort( 0x4 ),	/* x86 Stack size/offset = 4 */
+/* 28 */	0x8,		/* FC_LONG */
+			0x0,		/* 0 */
+
+	/* Parameter yPos */
+
+/* 30 */	NdrFcShort( 0x48 ),	/* Flags:  in, base type, */
+/* 32 */	NdrFcShort( 0x8 ),	/* x86 Stack size/offset = 8 */
+/* 34 */	0x8,		/* FC_LONG */
+			0x0,		/* 0 */
+
+	/* Parameter sideSize */
+
+/* 36 */	NdrFcShort( 0x48 ),	/* Flags:  in, base type, */
+/* 38 */	NdrFcShort( 0xc ),	/* x86 Stack size/offset = 12 */
+/* 40 */	0x8,		/* FC_LONG */
+			0x0,		/* 0 */
+
+	/* Return value */
+
+/* 42 */	NdrFcShort( 0x70 ),	/* Flags:  out, return, base type, */
+/* 44 */	NdrFcShort( 0x10 ),	/* x86 Stack size/offset = 16 */
+/* 46 */	0x8,		/* FC_LONG */
+			0x0,		/* 0 */
+
+	/* Procedure Draw */
+
+/* 48 */	0x33,		/* FC_AUTO_HANDLE */
+			0x6c,		/* Old Flags:  object, Oi2 */
+/* 50 */	NdrFcLong( 0x0 ),	/* 0 */
+/* 54 */	NdrFcShort( 0x8 ),	/* 8 */
+/* 56 */	NdrFcShort( 0xc ),	/* x86 Stack size/offset = 12 */
+/* 58 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 60 */	NdrFcShort( 0x8 ),	/* 8 */
+/* 62 */	0x46,		/* Oi2 Flags:  clt must size, has return, has ext, */
+			0x2,		/* 2 */
+/* 64 */	0x8,		/* 8 */
+			0x45,		/* Ext Flags:  new corr desc, srv corr check, has range on conformance */
+/* 66 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 68 */	NdrFcShort( 0x1 ),	/* 1 */
+/* 70 */	NdrFcShort( 0x0 ),	/* 0 */
+
+	/* Parameter hDc */
+
+/* 72 */	NdrFcShort( 0xb ),	/* Flags:  must size, must free, in, */
+/* 74 */	NdrFcShort( 0x4 ),	/* x86 Stack size/offset = 4 */
+/* 76 */	NdrFcShort( 0x2 ),	/* Type Offset=2 */
+
+	/* Return value */
+
+/* 78 */	NdrFcShort( 0x70 ),	/* Flags:  out, return, base type, */
+/* 80 */	NdrFcShort( 0x8 ),	/* x86 Stack size/offset = 8 */
+/* 82 */	0x8,		/* FC_LONG */
+			0x0,		/* 0 */
+
+			0x0
+        }
+    };
+
+static const DrawerServer_MIDL_TYPE_FORMAT_STRING DrawerServer__MIDL_TypeFormatString =
+    {
+        0,
+        {
+			NdrFcShort( 0x0 ),	/* 0 */
+/*  2 */	
+			0x12, 0x0,	/* FC_UP */
+/*  4 */	NdrFcShort( 0x2 ),	/* Offset= 2 (6) */
+/*  6 */	
+			0x2a,		/* FC_ENCAPSULATED_UNION */
+			0x48,		/* 72 */
+/*  8 */	NdrFcShort( 0x4 ),	/* 4 */
+/* 10 */	NdrFcShort( 0x2 ),	/* 2 */
+/* 12 */	NdrFcLong( 0x48746457 ),	/* 1215587415 */
+/* 16 */	NdrFcShort( 0x8008 ),	/* Simple arm type: FC_LONG */
+/* 18 */	NdrFcLong( 0x52746457 ),	/* 1383359575 */
+/* 22 */	NdrFcShort( 0x8008 ),	/* Simple arm type: FC_LONG */
+/* 24 */	NdrFcShort( 0xffff ),	/* Offset= -1 (23) */
+
+			0x0
+        }
+    };
+
+
+/* Object interface: IUnknown, ver. 0.0,
+   GUID={0x00000000,0x0000,0x0000,{0xC0,0x00,0x00,0x00,0x00,0x00,0x00,0x46}} */
+
+
+/* Object interface: IDispatch, ver. 0.0,
+   GUID={0x00020400,0x0000,0x0000,{0xC0,0x00,0x00,0x00,0x00,0x00,0x00,0x46}} */
+
+
+/* Object interface: ISquareObject, ver. 0.0,
+   GUID={0xba0a089b,0xb7a5,0x4f3b,{0xaa,0x02,0x56,0x79,0x73,0x0c,0xbd,0x29}} */
+
+#pragma code_seg(".orpc")
+static const unsigned short ISquareObject_FormatStringOffsetTable[] =
+    {
+    (unsigned short) -1,
+    (unsigned short) -1,
+    (unsigned short) -1,
+    (unsigned short) -1,
+    0,
+    48
+    };
+
+static const MIDL_STUBLESS_PROXY_INFO ISquareObject_ProxyInfo =
+    {
+    &Object_StubDesc,
+    DrawerServer__MIDL_ProcFormatString.Format,
+    &ISquareObject_FormatStringOffsetTable[-3],
+    0,
+    0,
+    0
+    };
+
+
+static const MIDL_SERVER_INFO ISquareObject_ServerInfo = 
+    {
+    &Object_StubDesc,
+    0,
+    DrawerServer__MIDL_ProcFormatString.Format,
+    &ISquareObject_FormatStringOffsetTable[-3],
+    0,
+    0,
+    0,
+    0};
+CINTERFACE_PROXY_VTABLE(9) _ISquareObjectProxyVtbl = 
+{
+    &ISquareObject_ProxyInfo,
+    &IID_ISquareObject,
+    IUnknown_QueryInterface_Proxy,
+    IUnknown_AddRef_Proxy,
+    IUnknown_Release_Proxy ,
+    0 /* IDispatch::GetTypeInfoCount */ ,
+    0 /* IDispatch::GetTypeInfo */ ,
+    0 /* IDispatch::GetIDsOfNames */ ,
+    0 /* IDispatch_Invoke_Proxy */ ,
+    (void *) (INT_PTR) -1 /* ISquareObject::SetPosition */ ,
+    (void *) (INT_PTR) -1 /* ISquareObject::Draw */
+};
+
+
+static const PRPC_STUB_FUNCTION ISquareObject_table[] =
+{
+    STUB_FORWARDING_FUNCTION,
+    STUB_FORWARDING_FUNCTION,
+    STUB_FORWARDING_FUNCTION,
+    STUB_FORWARDING_FUNCTION,
+    NdrStubCall2,
+    NdrStubCall2
+};
+
+CInterfaceStubVtbl _ISquareObjectStubVtbl =
+{
+    &IID_ISquareObject,
+    &ISquareObject_ServerInfo,
+    9,
+    &ISquareObject_table[-3],
+    CStdStubBuffer_DELEGATING_METHODS
+};
+
+static const MIDL_STUB_DESC Object_StubDesc = 
+    {
+    0,
+    NdrOleAllocate,
+    NdrOleFree,
+    0,
+    0,
+    0,
+    0,
+    0,
+    DrawerServer__MIDL_TypeFormatString.Format,
+    1, /* -error bounds_check flag */
+    0x60001, /* Ndr library version */
+    0,
+    0x801026e, /* MIDL Version 8.1.622 */
+    0,
+    0,
+    0,  /* notify & notify_flag routine table */
+    0x1, /* MIDL flag */
+    0, /* cs routines */
+    0,   /* proxy/server info */
+    0
+    };
+
+const CInterfaceProxyVtbl * const _DrawerServer_ProxyVtblList[] = 
+{
+    ( CInterfaceProxyVtbl *) &_ISquareObjectProxyVtbl,
+    0
+};
+
+const CInterfaceStubVtbl * const _DrawerServer_StubVtblList[] = 
+{
+    ( CInterfaceStubVtbl *) &_ISquareObjectStubVtbl,
+    0
+};
+
+PCInterfaceName const _DrawerServer_InterfaceNamesList[] = 
+{
+    "ISquareObject",
+    0
+};
+
+const IID *  const _DrawerServer_BaseIIDList[] = 
+{
+    &IID_IDispatch,
+    0
+};
+
+
+#define _DrawerServer_CHECK_IID(n)	IID_GENERIC_CHECK_IID( _DrawerServer, pIID, n)
+
+int __stdcall _DrawerServer_IID_Lookup( const IID * pIID, int * pIndex )
+{
+    
+    if(!_DrawerServer_CHECK_IID(0))
+        {
+        *pIndex = 0;
+        return 1;
+        }
+
+    return 0;
+}
+
+const ExtendedProxyFileInfo DrawerServer_ProxyFileInfo = 
+{
+    (PCInterfaceProxyVtblList *) & _DrawerServer_ProxyVtblList,
+    (PCInterfaceStubVtblList *) & _DrawerServer_StubVtblList,
+    (const PCInterfaceName * ) & _DrawerServer_InterfaceNamesList,
+    (const IID ** ) & _DrawerServer_BaseIIDList,
+    & _DrawerServer_IID_Lookup, 
+    1,
+    2,
+    0, /* table of [async_uuid] interfaces */
+    0, /* Filler1 */
+    0, /* Filler2 */
+    0  /* Filler3 */
+};
+#pragma optimize("", on )
+#if _MSC_VER >= 1200
+#pragma warning(pop)
+#endif
+
+
+#endif /* !defined(_M_IA64) && !defined(_M_AMD64) && !defined(_ARM_) */
+
